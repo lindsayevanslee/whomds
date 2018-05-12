6fef5c6354ee0f65b10d8f714aecdd47fee6ba4a
Lindsay Lee <yasdnil922@comcast.net>
2018-05-12 17:16:21 +0200

    Add test

--- tests/testthat/test-dimensions.R
+++ tests/testthat/test-dimensions.R
@@ -9,3 +9,28 @@ test_that("Dimensions as number of levels and 2", {
                )))
   expect_equal(ncol(tab), 2)
 })
+
+
+test_that("Dimensions as multiple of formula_vars, levels, and by_vars", {
+  
+  formula_vars <- paste0("fa",1:10)
+  formula_vars_levels <-1:5
+  by_vars <- "sex"
+  
+  tab <- table_weightedpct(mdstest, 
+                           vars_ids = c("VARUNIT_N","enc_id"),
+                           vars_strata = "VARSTRAT_N",
+                           vars_weights = "Factor_Persona",
+                           formula_vars = formula_vars,
+                           formula_vars_levels = formula_vars_levels,
+                           by_vars = by_vars)
+  
+  expect_equal(nrow(tab),
+               length(formula_vars) * length(formula_vars_levels) * length(levels(dplyr::pull(mdstest,by_vars)))
+  )
+  expect_equal(ncol(tab), 4)
+  
+  
+  
+}
+)
