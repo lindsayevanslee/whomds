c65efb70c927c6b56b20ecc7e1f6887cd810108e
Lindsay Lee <yasdnil922@comcast.net>
2018-05-15 16:37:18 +0200

    Edit description and startup message

---
+++ R/zzz.R
@@ -0,0 +1,3 @@
+.onAttach <- function(libname, pkgname) {
+  packageStartupMessage("Thank you for using whomds. To learn more about the WHO Model Disability Survey (MDS) and for contact information, go to http://www.who.int/disabilities/data/mds/en/")
+}
