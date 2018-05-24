45c8866ccf847ef81147480f07463a0e162e694b
Lindsay Lee <yasdnil922@comcast.net>
2018-05-24 15:31:35 +0200

    Update wrapper function to print final data

--- R/rasch_mds.R
+++ R/rasch_mds.R
@@ -6,15 +6,15 @@
 #' @param vars_DIF a string with the column names to use for analyzing differential item functioning (DIF). Default is NULL, to skip analysis of DIF.
 #' @param resp_opts a numeric vector of possible response options for \code{vars_metric}. Must begin with 1. Default is 1:5
 #' @param max_NA a numeric value for the maximum number of NAs allowed per individual among \code{vars_metric}. Default is 2.
+#' @param print_results a logical value indicating whether or not to print various files displaying results from the Rasch Model. Default is TRUE, to print the files.
+#' @param path_parent a string with the path to the folder where results from multiple models will be outputted. Default is NULL
+#' @param model_name a string with a name for the model, which is used to create a new folder for model output. Default is NULL.
 #' @param testlet_strategy a list giving the strategy to take for creating testlets, passed to \code{rasch_testlet()}. One element of the list per testlet to create. Each element of the list must be a character vector of column names to use for the testlet. Optionally, name the element of the list to give the name of the new testlet. Otherwise, the new testlet will be the original column names separated by "_". Default is NULL, to not create testlets.
 #' @param recode_strategy a named list giving the strategy to take for recoding variables, passed to \code{rasch_recode()}. One element of the list per recode strategy. Each element of the list is a numeric vector giving the new values to map the variables to. The names of the list are the groups of column names to use for each recoding strategy, separated only by ",". Default is NULL, to not recode items.
 #' @param drop_vars a character vector of column names to drop from the Rasch Analysis. Default is NULL, to not drop items.
 #' @param split_strategy a named list giving the strategy to take for spliting variables by categories, passed to \code{rasch_split()}. One element of the list per variable to split by. Each element of the list must be a character vector of column names to split. The names of the list are the variables to split each group of variables by. Default is NULL, to not split items.
 #' @param comment a string giving a comment describing the analysis, printed to a txt file. Default is NULL, to not print a comment.
-#' @param print_results a logical value indicating whether or not to print various files displaying results from the Rasch Model. Default is TRUE, to print the files.
-#' @param model_name a string with a name for the model, which is used to create a new folder for model output. Default is NULL.
-#' @param path_parent a string with the path to the folder where results from multiple models will be outputted. Default is NULL
-#'
+
 #' @details This function combines all of the separate analyses of model fit necessary to assess the quality of the Rasch Model. It is designed to require minimal intervention from the user. Users wishing to have more control over the analysis can use the other Rasch functions in this package separately.
 #' 
 #' @return a tibble with new columns representing the original person abilities (\code{person_pars}) and the rescaled person abilities (\code{rescaled}). 
@@ -32,15 +32,15 @@ rasch_mds <- function(df,
                       vars_DIF = NULL,
                       resp_opts = 1:5,
                       max_NA = 2,
+                      print_results = TRUE,
+                      path_parent = NULL,
+                      model_name = NULL,
                       testlet_strategy = NULL, 
                       recode_strategy = NULL, 
                       drop_vars = NULL, 
                       split_strategy = NULL,
-                      comment = NULL,
-                      print_results = TRUE ,
-                      model_name = NULL,
-                      path_parent = NULL
-                      ) {
+                      comment = NULL
+) {
   
   # # examples:
   # testlet_strategy <- list(mobility = c("c1","c2"), c("c3","c4","c5"))
@@ -216,6 +216,9 @@ rasch_mds <- function(df,
   df_final <- rasch_rescale(df = df,
                             df_score = df_score,
                             vars_id = vars_id)
+  
+  # PRINT DATA ---------
+  if (print_results) readr::write_csv(df_final, file = paste0(path_output, "Data_final.csv"))
   
   
   # RETURN DATA WITH SCORE ----------
