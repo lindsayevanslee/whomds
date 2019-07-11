35d85799c092b38175c6d71bccfbdd2a92cbda8b
Lindsay Lee <yasdnil922@comcast.net>
2019-07-11 12:15:53 +0200

    VERSION 0.0.0.9004 First try at fig_density function

---
+++ R/fig_density.R
@@ -0,0 +1,99 @@
+#' Plot a density of a score
+#'
+#' @param df a data frame where each row is an individual, containing at least a score column (between 0 and 100)
+#' @param score a string (length 1) of the column name for the score variable to print the distribution of
+#' @param var_color a string (length 1) of the column name for the variable to set color of density lines by. Default is NULL.
+#' @param var_facet a string (length 1) of the column name for the variable to create a \code{ggplot2::facet_grid()} with. Default is NULL.
+#' @param cutoffs a numeric vector of the cut-offs for the score categorization. Default is NULL.
+#' @param x_lab a string (length 1) of x-axis label. Default is "Score".
+#' @param pal a string specifying either a manual color to use for the color aesthetic, or as the name of a palette to pass to \code{RColorBrewer::brewer.pal()} with the name of the color palette to use. Default is \code{"Paired"}
+#' @param adjust a numeric value to pass to \code{adjust} argument of \code{ggplot2::geom_density()}. Default is 2.
+#' @param size a numeric value to pass to \code{size} argument of \code{ggplot2::geom_density()}. Default is 1.5.
+#' 
+#' @return A density figure
+#' 
+#' @details Plots a histogram of a score that ranges between 0 and 100, with the fill determined by some set categorization of the score. This is the function used to plot the distributions of disability scores resulting from the WHO Model Disability Survey.
+#' 
+#' @family figure functions
+#' @export 
+#' 
+#' @import ggplot2
+#' @import dplyr
+#'
+#' @examples
+#' fig_density(chile_adults, score = "PerformanceScorePredicted", cutoffs = c(19.1, 34.4, 49.6), x_lab = "Disability score")
+#' fig_density(chile_adults, score = "PerformanceScorePredicted", var_color = "sex", cutoffs = c(19.1, 34.4, 49.6), x_lab = "Disability score")
+#' fig_density(chile_adults, score = "PerformanceScorePredicted", var_facet = "sex",  cutoffs = c(19.1, 34.4, 49.6), x_lab = "Disability score")
+fig_density <- function(df, score, var_color = NULL, var_facet = NULL,
+                        cutoffs = NULL, x_lab = "Score", 
+                        pal = "Paired", adjust = 2, size = 1.5){
+  
+  #convert to tibble
+  if (!tibble::is_tibble(df)) df <- df %>% as_tibble()
+  
+
+  
+  
+  
+  #Initialize plot
+  if (is.null(var_color)) {
+    plot_density <- ggplot(df, aes(x = !!rlang::sym(score))) 
+      
+  } else {
+    plot_density <- ggplot(df, aes(x = !!rlang::sym(score), 
+                                   color = !!rlang::sym(var_color))) 
+  }
+  
+  
+  #Continue plot with formatting
+  plot_density <- plot_density + 
+    geom_density(adjust = adjust, size = size) +
+    scale_x_continuous(limits = c(min(-2.5, min(cutoffs) ), 102.5)) +
+    labs(x = "Score") +
+    theme(
+      panel.background = element_rect(fill = "transparent"), # bg of the panel
+      panel.grid.major = element_line(colour = "grey90"),
+      panel.grid.minor = element_blank(), # get rid of minor grid
+      
+      legend.background = element_rect(fill = "transparent"), # get rid of legend bg
+      legend.title = element_blank(),
+      legend.position="top",
+      legend.box = "horizontal",
+      legend.text = element_text(size = 12),
+      
+      plot.background = element_rect(fill = "transparent", color = NA), # bg of the plot
+      plot.margin = unit(c(1, 1, 3, 1), "lines"),
+      
+      axis.text.y = element_text(size = 12, colour = "black"),
+      axis.text.x = element_text(size = 12, colour = "black", angle = 0, hjust = 0.5, vjust = 0.5),
+      axis.title.y = element_text(size = 14),
+      axis.title.x = element_text(size = 14)
+      
+    ) 
+  
+  
+  #set colors
+  if (pal %in% rownames(RColorBrewer::brewer.pal.info)) {
+    plot_density <- plot_density +
+      scale_color_brewer(palette = pal)
+  } else {
+    plot_density <- plot_density +
+      scale_color_manual(values = pal) #doesn't work.....
+  }
+  
+  
+  #add cutoffs lines if non-NULL
+  if (!is.null(cutoffs)) {
+    plot_density <- plot_density + 
+      geom_vline(xintercept = cutoffs, color="black", linetype="dashed", size=.5)
+  }
+  
+  #add facet grid if var_facet non-NULL
+  if (!is.null(var_facet)) {
+    plot_density <- plot_density +
+      facet_grid(formula(paste("~", var_facet)))
+  }
+  
+ 
+  return(plot_density)
+}
