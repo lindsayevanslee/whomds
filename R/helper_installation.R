16faa6c51904a8c2ba2d4bb04dfa6e449f6d4b19
Lindsay Lee <yasdnil922@comcast.net>
2019-07-21 11:15:07 +0200

    Create function to check installation

---
+++ R/helper_installation.R
@@ -0,0 +1,35 @@
+#' Check installation of whomds is the most updated
+#'
+#' @return Prints a message stating whether or not installed package is same as most updated version from Github
+#'
+#' @examples whomds:::helper_installation()
+helper_installation <- function() {
+  
+  #capture last commit time
+  commit <- httr::content(httr::GET("https://api.github.com/repos/lindsayevanslee/whomds/git/refs/heads/master"))
+  commit_info <- httr::content(httr::GET(commit$object$url))
+  
+  last_commit_datetime <- lubridate::as_datetime(commit_info$author$date)
+  
+  #capture date of installation of package
+  install_datetime <- utils::packageDescription("whomds", fields = "Built")
+  install_datetime <- stringr::str_split(install_datetime, "; ") 
+  install_datetime <- base::unlist(install_datetime)
+  install_datetime <- base::suppressWarnings(lubridate::as_datetime(install_datetime))
+  install_datetime <- stats::na.exclude(install_datetime)
+  
+  #compare installation and commit dates and change startup message accordingly
+  if (last_commit_datetime > install_datetime) {
+    
+    message("There is a more updated version of the package available on github. You may want to reinstall this package to update it.")
+    
+  } else {
+    
+    message("Installed version is most updated.")
+  }
+  
+  
+  
+}
+
+
