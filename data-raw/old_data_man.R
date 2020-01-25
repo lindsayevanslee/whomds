a69018b6a03e8772c56b5aba28f1f51be2299eb6
Lindsay Lee <yasdnil922@comcast.net>
2020-01-25 19:01:02 +0100

    Add new sample data

---
+++ data-raw/old_data_man.R
@@ -0,0 +1,55 @@
+
+# # #' Example of WHO Model Disability Survey data for adults from Costa Rica
+# # #'
+# # #' An example data set of data from the WHO Model Disability Survey. The data is taken from the Costa Rica survey ENADIS (2018)
+# # #'
+# # #' For full details about the data, including variable descriptions, please go to \url{http://sistemas.inec.cr/pad4/index.php/catalog/218}
+# # #'
+# # #' @source Instituto Nacional de Estadística y Censos (INEC) de Costa Rica, WHO Disability Programme (leel@@who.int)
+# "costa_rica"
+# 
+# # #' Example of WHO Model Disability Survey data for adults from Chile
+# # #'
+# # #' An example data set of data from the WHO Model Disability Survey. The data is taken from the Chilean survey ENDIS II (2014)
+# # #'
+# # #' @format A tibble with 12256 rows and 136 variables:
+# #' \describe{
+# #'   \item{enc_id}{household number}
+# #'   \item{sex}{sex, Male or Female}
+# #'   \item{edad}{age in years}
+# #'   \item{age_cat}{age catgory, either "18-39y", "40-59y", or "60+y"}
+# #'   \item{work_cat}{whether respondent is currently working, binary}
+# #'   \item{edu_cat}{highest level of education attained}
+# #'   \item{marital_status}{marital status}
+# #'   \item{capacity_cat}{capacity level, as defined by WHO cutoffs of CapacityScore}
+# #'   \item{performance_cat}{performance level, as definted by WHO cutoffs of PerformanceScorePredicted}
+# #'   \item{CapacityScore}{metric scale of capacity, from Rasch analysis}
+# #'   \item{PerformanceScorePredicted}{metric scale of performance, from Rasch analysis and random forest regression}
+# #'   \item{VARUNIT_N}{cluster id}
+# #'   \item{VARSTRAT_N}{survey strata}
+# #'   \item{Factor_Persona}{survey weights}
+# #'   \item{c....}{survey item from "CAPACIDAD" section (see Chile ENDIS II questionnaire)}
+# #'   \item{d....}{survey item from "DESEMPENO" section (see Chile ENDIS II questionnaire)}
+# #'   \item{f....}{survey item from "FACTORES AMBIENTALES" section (see Chile ENDIS II questionnaire)}
+# #' }
+# #' @source WHO Disability Programme (leel@@who.int), El Servicio Nacional de la Discapacidad de Chile
+# "chile_adults"
+# 
+# #' Example of WHO Model Disability Survey data for children from Chile
+# #'
+# #' An example data set of data from the WHO Model Disability Survey. The data is taken from the Chilean survey ENDIS II (2014)
+# #'
+# #' @format A tibble with 5515 rows and 199 variables:
+# #' \describe{
+# #'   \item{enc_id}{household number}
+# #'   \item{sex}{sex, Male or Female}
+# #'   \item{edad}{age in years}
+# #'   \item{age_cat}{age catgory, either "Age2to4",  "Age5to9" or "Age10to17"}
+# #'   \item{VARUNIT_N}{cluster id}
+# #'   \item{VARSTRAT_N}{survey strata}
+# #'   \item{Factor_Persona}{survey weights}
+# #'   \item{e....}{survey item from "EDUCACION" section (see Chile ENDIS II questionnaire)}
+# #'   \item{n....}{survey item from "CUESTIONARIO INFANTIL" (see Chile ENDIS II questionnaire)}
+# #' }
+# #' @source WHO Disability Programme (leel@@who.int), El Servicio Nacional de la Discapacidad de Chile
+# "chile_children"
--- data-raw/sample_data.R
+++ data-raw/sample_data.R
@@ -1,5 +1,13 @@
 library(tidyverse)
+library(haven)
 
+#costa rica
+costa_rica <- haven::read_sav("/Users/lindsaylee/Dropbox/WHO/MDS/09 national and regional surveys/Costa Rica 2018/01 Data/ENADIS 2018 USUARIO.sav")
+
+# usethis::use_data(costa_rica, overwrite = TRUE)
+
+
+#chile
 load("/Users/lindsaylee/Dropbox/WHO/MDS/09 national and regional surveys/Chile 2014/04 Syntax/DescriptiveAnalysis/chile.RData")
 
 demos <- c("sex","edad","age_cat", "work_cat","edu_cat","marital_status")
@@ -24,16 +32,74 @@ chile_children <- data_total %>%
   ),
   sex = factor(sexo, levels = 1:2, labels = c("Male", "Female"))) %>% 
   select(c("enc_id", "sex", "edad", "age_cat", "VARUNIT_N", "VARSTRAT_N", "Factor_Persona"), 
-         matches("e\\d"), matches("n[1-5][0-9]"), matches("n[6][0-3]"), -contains("especifique")) %>% 
+         matches("e\\d"), matches("n[1-9]"), matches("n[1-5][0-9]"), matches("n[6][0-3]"), -contains("especifique")) %>% 
   map(function(x) {
     attr(x, "value.labels") <- NULL
     return(x)
   }) %>% 
   as_tibble()
-  
+
 
 rm(list=setdiff(ls(), c("chile_adults", "chile_children")))
 
-devtools::use_data(chile_adults, overwrite = TRUE)
-devtools::use_data(chile_children, overwrite = TRUE)
+# usethis::use_data(chile_adults, overwrite = TRUE)
+# usethis::use_data(chile_children, overwrite = TRUE)
+
+
+#anonymized
+
+set.seed(1992)
+df_adults <- tibble(HHID = 1:2500,
+                    strata = sample(c("A", "B", "C"), size = 2500, replace = TRUE),
+                    PSU = sample(1:20, size = 2500, replace = TRUE),
+                    weight = sample(seq(800, 1200, by = 50), size = 2500, replace = TRUE),
+                    sex = sample(c("Male", "Female"), size = 2500, replace = TRUE), 
+                    age = sample(18:100, size = 2500, prob = rbeta(83, 2, 5), replace = TRUE),
+                    age_cat = cut(
+                      age,
+                      breaks = c(18, 25, 40, 65, 100),
+                      labels = c("18-24", "25-39", "40-64", "64-100"),
+                      right = FALSE,
+                      include.lowest = TRUE,
+                      ordered_result = TRUE
+                    ),
+                    work_cat = sample(c("Y", "N"), size = 2500, prob = c(0.6, 0.4), replace = TRUE),
+                    edu_cat = sample(c("None", "Elementary", "Secondary", "University"), size = 2500, prob = c(0.2, 0.4, 0.25, 0.15), replace = TRUE)) %>%
+  bind_cols(
+    chile_adults %>% 
+      sample_n(2500) %>% 
+      select(d1:d47, c2:c25, fa1:fa12, CapacityScore, capacity_cat, PerformanceScorePredicted, performance_cat) %>% 
+      rename_at(.vars = vars(d1:d47), .funs = list(~ str_replace_all(., "d", "F"))) %>% 
+      rename_at(.vars = vars(c2:c25), .funs = list(~ str_replace_all(., "c", "C"))) %>% 
+      rename_at(.vars = vars(fa1:fa12), .funs = list(~ str_replace_all(., "fa", "EF"))) %>% 
+      rename(
+        capacity_score = CapacityScore,
+        disability_score = PerformanceScorePredicted,
+        disability_cat = performance_cat)
+  )
+
+
+df_children <- df_adults %>% 
+  select(HHID:weight) %>% 
+  mutate(sex = sample(c("Male", "Female"), size = 2500, replace = TRUE)) %>% 
+  bind_cols(
+    chile_children %>% 
+      sample_n(2500) %>% 
+      select(edad, age_cat, n1:n35) %>% 
+      rename_at(.vars = vars(n1:n35), .funs = list(~ str_replace_all(., "n", "child"))) %>% 
+      rename(age = edad) %>% 
+      mutate(age = case_when(
+        age_cat == "Age2to4" ~ sample(2:4, 1),
+        age_cat == "Age5to9" ~ sample(5:9, 1),
+        age_cat == "Age10to17" ~ sample(10:17, 1)
+      ),
+      age_cat = plyr::mapvalues(age_cat, 
+                                from = c("Age2to4", "Age5to9", "Age10to17"), 
+                                to = c("2-4", "5-9", "10-17")))
+    
+  ) %>% print()
+
+
+usethis::use_data(df_adults, overwrite = TRUE)
+usethis::use_data(df_children, overwrite = TRUE)
 
