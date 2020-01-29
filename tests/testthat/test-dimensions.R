4f5ff9e47126fd62dffb4d730a5dcde393202eaa
Lindsay Lee <yasdnil922@comcast.net>
2020-01-29 13:38:07 +0100

    Update tests

--- tests/testthat/test-dimensions.R
+++ tests/testthat/test-dimensions.R
@@ -1,11 +1,11 @@
 context("Dimensions of tables")
 
 test_that("Dimensions as number of levels and 4", {
-  tab <- table_unweightedpctn(chile_adults, c("sex", "edu_cat"))
+  tab <- table_unweightedpctn(df_adults, c("sex", "edu_cat"))
   
   expect_equal(nrow(tab),
                length(c(
-                 levels(chile_adults$sex), levels(chile_adults$edu_cat)
+                 levels(df_adults$sex), levels(df_adults$edu_cat)
                )))
   expect_equal(ncol(tab), 4)
 })
@@ -13,20 +13,20 @@ test_that("Dimensions as number of levels and 4", {
 
 test_that("Dimensions as multiple of formula_vars, levels, and by_vars", {
   
-  formula_vars <- paste0("fa",1:10)
+  formula_vars <- paste0("EF",1:10)
   formula_vars_levels <-1:5
   by_vars <- "sex"
   
-  tab <- table_weightedpct(chile_adults, 
-                           vars_ids = c("VARUNIT_N","enc_id"),
-                           vars_strata = "VARSTRAT_N",
-                           vars_weights = "Factor_Persona",
+  tab <- table_weightedpct(df_adults, 
+                           vars_ids = c("PSU","HHID"),
+                           vars_strata = "strata",
+                           vars_weights = "weight",
                            formula_vars = formula_vars,
                            formula_vars_levels = formula_vars_levels,
                            by_vars = by_vars)
   
   expect_equal(nrow(tab),
-               length(formula_vars) * length(formula_vars_levels) * length(levels(dplyr::pull(chile_adults,by_vars)))
+               length(formula_vars) * length(formula_vars_levels) * length(levels(dplyr::pull(df_adults,by_vars)))
   )
   expect_equal(ncol(tab), 4)
   
