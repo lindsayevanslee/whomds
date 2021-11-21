9e9d1b8f9b4bdb676903356eced88b03d0243346
Lindsay Lee <yasdnil922@comcast.net>
2021-11-21 11:37:49 -0600

    Update website in startup message

--- R/zzz.R
+++ R/zzz.R
@@ -1,3 +1,3 @@
 .onAttach <- function(libname, pkgname) {
-  packageStartupMessage("Thank you for using whomds. To learn more about the WHO Model Disability Survey (MDS) and for contact information, go to https://www.who.int/disabilities/data/en/")
+  packageStartupMessage("Thank you for using whomds. To learn more about the WHO Model Disability Survey (MDS) and for contact information, go to https://www.who.int/health-topics/disability")
 }
